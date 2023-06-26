29255f0e4719ac4c9b1139f63e6cea2ecd9e9746
HoOman <hooman202@hotmail.com>
2023-06-27 08:45:35 +0900

    Wake on lan
    Sender : wake on Lan
    
    Reciver : Put all the computers to Sleep

--- Sender-reciever/sender.py
+++ Sender-reciever/sender.py
@@ -1,4 +1,35 @@
 import socket
+import struct
+import codecs
+import binascii
+import time
+
+ip = '192.168.128.109'  # IP address of the specific computer
+broadcast_ip = '192.168.128.255' #ATC
+# broadcast_ip = '10.229.40.255' #Kyoto University
+port = 12345
+
+# Specify the MAC address of the sleeping computer
+mac_pc_dict = {}
+
+# Add MAC addresses and PC names to the dictionary
+mac_pc_dict[''] = 'PC01'
+mac_pc_dict[''] = 'PC02'
+mac_pc_dict['24-4B-FE-BC-4D-A9'] = 'PC03'
+mac_pc_dict['24-4B-FE-BC-43-83'] = 'PC04'
+mac_pc_dict[''] = 'PC05'
+mac_pc_dict[''] = 'PC06'
+mac_pc_dict[''] = 'PC07'
+mac_pc_dict[''] = 'PC08'
+mac_pc_dict['D4-93-90-20-92-78'] = 'PC09'
+mac_pc_dict['D4-93-90-20-93-B9'] = 'PC10'
+mac_pc_dict['D4-93-90-20-92-D0'] = 'PC11'
+mac_pc_dict[''] = 'PC12'
+mac_pc_dict[''] = 'PC13'
+mac_pc_dict[''] = 'PC14'
+
+
+
 
 def send_broadcast_message(message, ip, port):
     # Create a UDP socket
@@ -23,10 +54,19 @@ def send_message(message, ip, port):
         # Close the sender socket
         sock.close()
 
-ip = '192.168.128.109'  # IP address of the specific computer
-# broadcast_ip = '192.168.128.255' #ATC
-broadcast_ip = '10.229.40.255' #Kyoto University
-port = 12345
+def wake_on_lan(mac_address):
+    # Create a magic packet with the MAC address
+    mac_address = mac_address.replace('-', '')  # Replace dashes with colons
+    if len(mac_address) % 2 != 0:
+        mac_address = '0' + mac_address  # Prepend '0' if length is odd
+
+    mac_bytes = codecs.decode(mac_address.replace(':', ''), 'hex')
+    magic_packet = b'\xff' * 6 + mac_bytes * 16
+
+    # Send the magic packet to the broadcast address
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.sendto(magic_packet, ('<broadcast>', 9))
 
 # Usage example
 #message = "start_program"
@@ -43,9 +83,9 @@ print("-----------------------------")
 while True: 
     input_command = input("Please enter the command: ")
     if(input_command == "1"):
-        message = "turn_on"
-        send_broadcast_message(message, broadcast_ip, port)
-        print("Message sent - Turning on all PCs")
+        for mac_address, pc_name in mac_pc_dict.items():
+            wake_on_lan(mac_address)
+            print(f"Message sent - Waking up {pc_name} with Mac address {mac_address}")
     
     elif(input_command == "2"):
         message = "reset"
@@ -68,9 +108,12 @@ while True:
         print("Message sent - Turning off all PCs")
 
     elif(input_command == "6"):
-        message = "reset"
+        send_broadcast_message("kill_app", broadcast_ip, port)
+        print("Message sent - Killing Skeleton.exe on all PCs")
+        time.sleep(1)
+        message = "sleep"
         send_broadcast_message(message, broadcast_ip, port)
-        print("Message sent - Put all PCs to sleep")
+        print("Message sent 4- Put all PCs to sleep")
 
     elif(input_command == "7"):
         print("Which PC do you like to Talk with - not done")
